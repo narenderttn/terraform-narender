4d1bc0b9c69a955274b8886ebbf74edfe5081153
narenderttn <narender.panwar@tothenew.com>
2023-08-29 15:58:38 +0530

    test

--- _data.tf
+++ _data.tf
@@ -1,13 +1,13 @@
 data "archive_file" "start_scheduler" {
   type        = "zip"
-  source_file = "./python/start_instances.py"
-  output_path = "./python/start_instances.zip"
+  source_file = "../python/start_instances.py"
+  output_path = "../python/start_instances.zip"
 }
 
 data "archive_file" "stop_scheduler" {
   type        = "zip"
-  source_file = "./python/stop_instances.py"
-  output_path = "./python/stop_instances.zip"
+  source_file = "../python/stop_instances.py"
+  output_path = "../python/stop_instances.zip"
 }
 
 data "aws_iam_policy_document" "ec2_start_stop_scheduler" {
